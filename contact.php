da12d0d7fea2aa8a08452da13608e7614474a014
Mohamad Inalo <88008468+Mo-Inalo@users.noreply.github.com>
2023-04-17 02:04:14 +0200

    vid und timeline

--- contact.php
+++ contact.php
@@ -13,31 +13,25 @@ include "templates/nav.php"
 
 
 
-<div class="row pagecolor ">
+<div class="containercontact">
+    <form action="action_page.php" class="p-md-5">
 
-    <div  class="col-md-7 p-5">
-        <img class="rounded-3 img-fluid height:auto border-primary-subtle  " src="pictures/e2.jpg" alt="Elende Usiv"   >
-    </div>
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name..">
 
-    <div class=" col-md-5 h4 text-dark p-5">
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
 
+        <label for="country">Phone</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your phone number..">
 
-        Elende Usiv is a Kurdish composer and guitarist, born March 21, 1985, in Al-Hasakah, Syria. He lives in Germany
-        since 2009. Usiv studies musicology at the University of Cologne. Elende Usiv was a member of the folk music
-        ensemble at the University of Cologne. He was involved in a concert group in several German cities as part of
-        Kurdish folk music. In addition, Elende Usiv composed and arranged a collection of songs for many Kurdish singers,
-        as well as soundtracks for several films, such as E.g.: "Just Before the End" by Behzad. "Nipple Story" By Rodi Hameh.
-        He has worked as an assistant composer with the American composer (Jay Schwartz) in many orchestral works.
-        In 2021 he worked as an intern with the German composer (Olaf Didolf) in the famous German series (Tatort).
+        <label for="subject">Subject</label>
+        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
 
-        </p>
+        <input type="submit" value="Submit">
 
-    </div>
-
-
-
+    </form>
 </div>
-
 
 <?php
 include "templates/footer.php"
--- studio.php
+++ studio.php
@@ -2,44 +2,42 @@
 include "templates/header.php";
 ?>
 
+<video autoplay muted loop id="myVideo">
+    <source src="/videos/elend.mp4" type="video/mp4">
+    Your browser does not support HTML5 video.
+</video>
+
+<div class="content">
+     <a class="text-light h5" href="index.php" > back to home</a>
+    <p> I'm Elende Usiv, a Cologne-based composer and music editor for media who has worked on films and commercials.
+        You can hear some of my music on YouTube, Soundcloud and Spotify.
+        At the same time as my film music career, I composed and arranged many pop songs for various Kurdish singers.
+        who have supported my journey and career as a film music composer!
+    </p>
+    <button id="myBtn" onclick="myFunction()">Pause</button>
+</div>
+
+<script>
+    var video = document.getElementById("myVideo");
+    var btn = document.getElementById("myBtn");
+
+    function myFunction() {
+        if (video.paused) {
+            video.play();
+            btn.innerHTML = "Pause";
+        } else {
+            video.pause();
+            btn.innerHTML = "Play";
+        }
+    }
+</script>
+
+</body>
+</html>
 
 
-<?php
-include "templates/nav.php"
-?>
-
-<?php
-include "templates/breakingnews.php"
-?>
-
-<main>
-    <div class="row pagecolor ">
-
-        <div  class="col-md-7 p-5">
-            <img class="rounded-3 img-fluid height:auto border-primary-subtle  " src="pictures/e1.jpg" alt="Elende Usiv"   >
-        </div>
-
-        <div class=" col-md-5 h6 text-dark p-5">
-
-
-            <p class="h2 my-5 text-dark"> I'm Elende Usiv, a Cologne-based composer and music editor for media who has
-                worked on films and commercials.
-                You can hear some of my music on YouTube, Soundcloud and Spotify.
-                At the same time as my film music career, I composed and arranged many pop songs for various Kurdish singers.
-                who have supported my journey and career as a film music composer.
-            </p>
-
-        </div>
 
 
 
-    </div>
-</main>
-
-
-
-<?php
-include "templates/footer.php"
-?>
 
 

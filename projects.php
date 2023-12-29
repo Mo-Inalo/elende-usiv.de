1018921a75a9554420a2c16c3758103e5d6120e7
Mohamad Inalo <88008468+Mo-Inalo@users.noreply.github.com>
2023-12-29 02:51:50 +0100

    finale Abarbeitung2

--- projects.php
+++ projects.php
@@ -14,16 +14,15 @@ include "templates/nav.php"
 include "templates/breakingnews.php"
 ?>
 
-<style>
 
 
-</style>
-
-<main style="background-image: url('/elende-usiv.de/images/e3-opac.png')  " >
+<main style="background-image: url('/elende-usiv.de/images/e3-opac.png'); background-size: cover; background-attachment: fixed;background-position: center center;" >
 
 
-<div style=" padding-top: 30px; padding-bottom: 30px ">
-
+<div  >
+<br>
+<br>
+<br>
     <table class="table table-hover align-content-center">
 
         <thead>
@@ -60,21 +59,21 @@ include "templates/breakingnews.php"
 
         <tr>
             <th scope="row"> </th>
-            <td><a href="https://www.youtube.com/watch?v=JYh1wMtfE7Y"> A Nipple Story - Composer | Dir. Rodi Hameh (Dasra Film)</a></td>
+            <td><a href="https://www.youtube.com/watch?v=JYh1wMtfE7Y"> A Nipple Story | Dir. Rodi Hameh (Dasra Film)</a></td>
             <td> Composer </td>
             <td>2022</td>
         </tr>
 
         <tr>
             <th scope="row"> </th>
-            <td><a href="https://www.youtube.com/watch?v=ZEzIY8GP3bA&t=1s">16 SECONDS – Composer | Dir. Diya Al Abdullah (Kurdland 9) </a></td>
+            <td><a href="https://www.youtube.com/watch?v=ZEzIY8GP3bA&t=1s"> 16 SECONDS | Dir. Diya Al Abdullah (Kurdland 9) </a></td>
             <td> Composer </td>
             <td>2022</td>
         </tr>
 
         <tr>
             <th scope="row"> </th>
-            <td>Al Iada – Composer | Podcast by Rodi Hameh & Diya Alabdula (Kurdland9)</td>
+            <td>Al Iada | Podcast by Rodi Hameh & Diya Alabdula (Kurdland9)</td>
             <td> Composer </td>
             <td>2023</td>
         </tr>
@@ -87,86 +86,103 @@ include "templates/breakingnews.php"
     <table class="table table-hover align-content-center">
 
         <thead>
-        <tr>
-            <th scope="col"></th>
-            <th scope="col">Pop Songs</th>
-            <th scope="col">Singer</th>
-            <th scope="col">Role</th>
-            <th scope="col">Year</th>
-        </tr>
+            <tr>
+                <th scope="col"></th>
+                <th scope="col">Pop Songs</th>
+                <th scope="col">Singer</th>
+                <th scope="col">Role</th>
+                <th scope="col">Year</th>
+            </tr>
         </thead>
-
         <tbody  >
-        <tr>
-            <th scope="row"></th>
-            <td>Bo çi</td>
-            <td>Dilo Doxan</td>
-            <td>Composer</td>
-            <td>2016</td>
-        </tr>
-        <tr>
-            <th scope="row"></th>
-            <td>Resh u Sipî </td>
-            <td>Dilo Doxan</td>
-            <td>Composer</td>
-            <td>2017</td>
-        </tr>
+            <tr>
+                <th scope="row"></th>
+                <td>Bo çi</td>
+                <td>Dilo Doxan</td>
+                <td>Composer</td>
+                <td>2016</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td>Resh u Sipî </td>
+                <td>Dilo Doxan</td>
+                <td>Composer</td>
+                <td>2017</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td>Payiz bû</td>
+                <td>Rawan Îbrahim</td>
+                <td>Composer</td>
+                <td>2018</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td>Mîna Ba</td>
+                <td>Dilo Doxan</td>
+                <td>Arrangment</td>
+                <td>2019</td>
+            </tr>
 
+            <tr>
+                <th scope="row"></th>
+                <td>Dil ji min bir </td>
+                <td>Dilo Doxan</td>
+                <td>Composer</td>
+                <td>2021</td>
+            </tr>
 
-        <tr>
-            <th scope="row"></th>
-            <td>Payiz bû</td>
-            <td>Rawan Îbrahim</td>
-            <td>Composer</td>
-            <td>2018</td>
-        </tr>
-
-        <tr>
-            <th scope="row"> </th>
-            <td>The Black Flower </td>
-            <td> </td>
-            <td> Composer</td>
-            <td>2019</td>
-        </tr>
-
-        <tr>
-            <th scope="row"> </th>
-            <td>Just before the End </td>
-            <td> </td>
-            <td>2019</td>
-        </tr>
-
-
-        <tr>
-            <th scope="row"> </th>
-            <td><a href="https://www.youtube.com/watch?v=JYh1wMtfE7Y"> A Nipple Story - Composer | Dir. Rodi Hameh (Dasra Film)</a></td>
-            <td> Composer </td>
-            <td>2022</td>
-        </tr>
-
-        <tr>
-            <th scope="row"> </th>
-            <td><a href="https://www.youtube.com/watch?v=ZEzIY8GP3bA&t=1s">16 SECONDS – Composer | Dir. Diya Al Abdullah (Kurdland 9) </a></td>
-            <td> Composer </td>
-            <td>2022</td>
-        </tr>
-
-        <tr>
-            <th scope="row"> </th>
-            <td>Al Iada – Composer | Podcast by Rodi Hameh & Diya Alabdula (Kurdland9)</td>
-            <td> Composer </td>
-            <td>2023</td>
-        </tr>
+            <tr>
+                <th scope="row"></th>
+                <td>Canê te Dilê min Sot </td>
+                <td>Dilo Doxan</td>
+                <td>Arrangment</td>
+                <td>2021</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td>Tê Bîra te</td>
+                <td>Eylem Derkî</td>
+                <td>Composer</td>
+                <td>2022</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td>Gundê em lê </td>
+                <td>Dilo Doxan</td>
+                <td>Arrangment</td>
+                <td>2022</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td>Xewnikê </td>
+                <td>Gulav Omerî</td>
+                <td>Composer</td>
+                <td>2022</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td>Bastan</td>
+                <td>Eylem Derkî</td>
+                <td>Composer</td>
+                <td>2022</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td>Ji te Hezdikim  </td>
+                <td>Dilo Doxan</td>
+                <td>Arrangment</td>
+                <td>2022</td>
+            </tr>
         </tbody>
 
     </table>
 </div>
-
+    <br>
+    <br>
+    <br>
 
 </main>
-
-
-
 
 <?php
 include "templates/footer.php"

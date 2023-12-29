80dcd42dfb2d29e297864a4bcfbb0684076e8b7f
Mohamad Inalo <88008468+Mo-Inalo@users.noreply.github.com>
2023-12-29 22:08:33 +0100

    finale Abarbeitung 14

--- templates/breakingnews.php
+++ templates/breakingnews.php
@@ -6,7 +6,7 @@
     </div>
 
     <div class="col-4" align="right">
-        <h6  align="right" style="padding: 7px;" >
+        <h6  align="right" style="padding-top:6px;" >
             &#9835;  It's out! check my <a class=" text-white-50" href="https://www.youtube.com/watch?v=osVSP4WzVrg" target="_blank" rel="noopener noreferrer" >new</a> Album now.  &#9834;
         </h6>
     </div>

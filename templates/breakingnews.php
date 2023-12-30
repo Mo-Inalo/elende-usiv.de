59e91e42b05d3d7c7f635a0c565f941f3c25ad92
Mohamad Inalo <88008468+Mo-Inalo@users.noreply.github.com>
2023-12-30 03:44:14 +0100

    finale Abarbeitung 24

--- templates/breakingnews.php
+++ templates/breakingnews.php
@@ -1,21 +1,16 @@
 
 
 <div class="row my-3 Elendeauswahl" style="background-color: #545556">
-    <div class="col-4" >
 
-    </div>
-
-    <div class="col-4" align="left">
-        <h6  align="right" style="padding-top:6px;" >
+    <div class="row-cols-3 " align="center">
+        <p  align="center" style="padding-top:6px;" >
             &#9835;  It's out! check my <a class=" text-white-50" href="https://www.youtube.com/watch?v=osVSP4WzVrg" target="_blank" rel="noopener noreferrer" >new</a> Album now.  &#9834;
-        </h6>
+        </p>
     </div>
-    <div class="col-4" align="left">
+    <div class="row-cols-3" align="center">
         <audio   src="/music/winter.mp3" controls="controls" loop="loop" preload="auto"></audio>
     </div>
-    <div class="col-4" >
 
-    </div>
 </div>
 
 

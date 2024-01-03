ebf0583cadefc60c679710a3152df7e60c5c8cce
Mohamad Inalo <88008468+Mo-Inalo@users.noreply.github.com>
2024-01-03 23:22:46 +0100

    form edited

--- contact.php
+++ contact.php
@@ -13,15 +13,19 @@ include "templates/nav.php"
 
 
 
-<div class="containercontact" style="background: url('/elende-usiv.de/images/entfer.png')">
+<div class="containercontact" style="background: url('/images/entfer.png')">
+
     <form action="https://formsubmit.co/musicelend@hotmail.com" method="POST" class="p-md-5">
 
         <select name="title" required>
-            <option>Title </option>
+            <option>Select a Title </option>
             <option>Mr </option>
             <option>Mrs</option>
             <option>Mx</option>
-
+            <option>Dr</option>
+            <option>Prof</option>
+            <option>Prof Dr</option>
+            <option>Other ..</option>
         </select>
 
 
@@ -35,10 +39,16 @@ include "templates/nav.php"
         <label for="tele">Phone</label>
         <input type="text" id="tele" name="tele" placeholder="Please insert your phone number.." required>
 
+        <label for="email">E-Mail</label>
+        <input type="text" id="email" name="email" placeholder="Please insert your E-Mail .." required>
+
+        <label for="adress">Adress</label>
+        <input type="text" id="adress" name="adress" placeholder="Please insert your Adress ..">
+
         <label for="subject">Subject</label>
         <textarea id="subject" name="subject" placeholder="Please write here your message .. " style="height:200px" required></textarea>
 
-        <input type="submit" value="Submit" style="background: rgba(9,154,18,0.45)">
+        <input type="submit" value="Submit" style="background: rgba(44,44,44,0.78)">
 
     </form>
 </div>

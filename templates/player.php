1dcdb13666490e8837c629d3648eca2937def6bf
Mohamad Inalo <88008468+Mo-Inalo@users.noreply.github.com>
2024-01-03 21:37:39 +0100

    Update player.php

--- templates/player.php
+++ templates/player.php
@@ -1,4 +1,4 @@
 
 <div  align="center" >
-    <audio src="/music/winter.mp3" controls="controls" style="width: 80%"></audio>
+    <audio src="/music/winter.mp3" controls="controls" style="width: 95%"></audio>
 </div>

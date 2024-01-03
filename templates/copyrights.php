96f2f8fd64c3cde21b9c27b2af05c35c2f80393c
Mohamad Inalo <88008468+Mo-Inalo@users.noreply.github.com>
2024-01-03 23:49:13 +0100

    footer edited

--- templates/copyrights.php
+++ templates/copyrights.php
@@ -2,9 +2,18 @@
 
 <!-- Copyright -->
 <div class="text-center text-dark p-2 copyright" style="font-weight: bold">
-    2024 - Copyright ©
-    <a href="http://inalo.me/"  >
-        <img src="/images/inalologo.png" style="width: 70px; height: 30px">
+    <span id="year"></span><span>- Copyright ©</span>
+    <a href="http://inalo.me/" target="_blank" rel="noopener noreferrer" >
+        <img src="/images/inalologo.png" style="width: 70px; height: 30px" alt="">
     </a>
 </div>
+
+
+
+
+<script>
+    const d = new Date();
+    let year = d.getFullYear();
+    document.getElementById("year").innerHTML = year;
+</script>
 <!-- Copyright -->
--- templates/socialmediaicons.php
+++ templates/socialmediaicons.php
@@ -6,35 +6,51 @@
         <a
             class="btn btn-link btn-floating btn-lg socialmeidachannelsitemcolor m-1"
             href="https://www.facebook.com/elende.usiv"
+            target="_blank" rel="noopener noreferrer"
             role="button"
             data-mdb-ripple-color="dark"
         ><i class="fab fa-facebook-f"></i
             ></a>
 
 
-        <!-- Twitter
+        <!-- soundcloud -->
         <a
             class="btn btn-link btn-floating btn-lg socialmeidachannelsitemcolor m-1"
-            href="#!"
+            href="https://soundcloud.com/user-32432553-468769487"
             role="button"
+            target="_blank" rel="noopener noreferrer"
             data-mdb-ripple-color="dark"
-        ><i class="fab fa-twitter"></i
-            ></a>
-        -->
-        <!-- Google
+        ><i class="fab fa-soundcloud"></i
+        ></a>
+
+        <!-- twitter -->
+       <a
+           class="btn btn-link btn-floating btn-lg socialmeidachannelsitemcolor m-1"
+           href="https://x.com/elendeusiv?s=20"
+           role="button"
+           target="_blank" rel="noopener noreferrer"
+           data-mdb-ripple-color="dark"
+       ><i class="fab fa-twitter"></i
+       ></a>
+
+
+
+        <!-- amazon -->
         <a
             class="btn btn-link btn-floating btn-lg socialmeidachannelsitemcolor m-1"
-            href="#!"
+            href="https://www.amazon.de/music/player/artists/B08VGY3LX9/elende-usiv"
             role="button"
+            target="_blank" rel="noopener noreferrer"
             data-mdb-ripple-color="dark"
-        ><i class="fab fa-google"></i
+        ><i class="fab fa-amazon"></i
             ></a>
-         -->
+
         <!-- Instagram -->
         <a
             class="btn btn-link btn-floating btn-lg socialmeidachannelsitemcolor m-1"
             href="https://www.instagram.com/elende.usiv/"
             role="button"
+            target="_blank" rel="noopener noreferrer"
             data-mdb-ripple-color="dark"
         ><i class="fab fa-instagram"></i
             ></a>
@@ -44,6 +60,7 @@
             class="btn btn-link btn-floating btn-lg socialmeidachannelsitemcolor m-1"
             href="https://www.youtube.com/@elendeusiv7372"
             role="button"
+            target="_blank" rel="noopener noreferrer"
             data-mdb-ripple-color="dark"
         ><i class="fab fa-youtube"></i
             ></a>
@@ -52,14 +69,16 @@
             class="btn btn-link btn-floating btn-lg socialmeidachannelsitemcolor m-1"
             href="https://open.spotify.com/intl-de/artist/3PHjtebOPjk5Dzrje9Ck2U?si=gUQZJabrTDi_RhthSpDjCg"
             role="button"
+            target="_blank" rel="noopener noreferrer"
             data-mdb-ripple-color="dark"
         ><i class="fab fa-spotify"></i
             ></a>
         <!-- apple -->
         <a
             class="btn btn-link btn-floating btn-lg socialmeidachannelsitemcolor m-1"
-            href="#!"
+            href=""
             role="button"
+            target="_blank" rel="noopener noreferrer"
             data-mdb-ripple-color="dark"
         ><i class="fab fa-apple"></i
             ></a>
@@ -69,6 +88,7 @@
             class="btn btn-link btn-floating btn-lg socialmeidachannelsitemcolor m-1"
             href="https://msng.link/o/?https://t.me/Elende_usiv=tg"
             role="button"
+            target="_blank" rel="noopener noreferrer"
             data-mdb-ripple-color="dark"
         ><i class="fab fa-telegram"></i
             ></a>

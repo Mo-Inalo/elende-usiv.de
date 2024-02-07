869b6ddcb3982a5f4f563f87795ef38afa1619c9
Mohamad Inalo <88008468+Mo-Inalo@users.noreply.github.com>
2024-02-07 20:52:57 +0100

    finalisiert

--- templates/breakingnews.php
+++ templates/breakingnews.php
@@ -21,7 +21,7 @@
           <h6  style="padding: 3px">
            <!-- &#9835; -->  It's out! check my album n
               <i class="bi bi-play-circle" id="playbutton">
-                  <a   style ="margin: -3px" class="text-dark " href="https://www.youtube.com/watch?v=osVSP4WzVrg" target="_blank" rel="noopener noreferrer" >
+                  <a   style ="margin: -3px" class="text-dark " href="https://www.youtube.com/watch?v=ARt1x5-Jptg" target="_blank" rel="noopener noreferrer" >
                       <svg   xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
                           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                           <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
